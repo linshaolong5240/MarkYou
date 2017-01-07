c9d57476d196701b885051912b9832b066d22c6e
mark <634205468@qq.com>
2017-01-07 23:08:57 +0800

    add 404 500

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from config import load_config
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 config = load_config()
 app.config.from_object(config)
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -17,6 +22,22 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',user_name=name)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.run()
--- config/config_default.py
+++ config/config_default.py
@@ -1,2 +1,5 @@
 class ConfigDefault:
     DEBUG = True
+    SECRET_KEY = 'Mark You' 
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:594120@localhost/database'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
