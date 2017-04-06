65bc200238c7ef23a84588bf781bcac6ff9d4912
Mark <634205468@qq.com>
2017-04-06 17:06:47 +0800

    1.add role model 2.add permission

--- app/models.py
+++ app/models.py
@@ -4,6 +4,50 @@ from flask_login import UserMixin
 from . import db
 from . import login_manager
 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+class Role(db.Model,UserMixin):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key =True)
+    name = db.Column(db.Unicode(128),unique=True)
+    default = db.Column(db.Boolean, default=True, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+                'User'          : (Permission.FOLLOW|
+                                   Permission.COMMENT|
+                                   Permission.WRITE_ARTICLES,
+                                   True),
+                'Moderator'     : (Permission.FOLLOW|
+                                   Permission.COMMENT|
+                                   Permission.WRITE_ARTICLES|
+                                   Permission.MODERATE_COMMENTS,
+                                   False),
+                'Administrator' : (0xFF,
+                                   False),
+                }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+            db.session.commit()
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 class User(db.Model,UserMixin):
     __tablename__ = 'users'
@@ -11,9 +55,14 @@ class User(db.Model,UserMixin):
     username = db.Column(db.Unicode(128), unique=True)
     email = db.Column(db.Unicode(128), unique=True)
     password_hash = db.Column(db.Unicode(128))
-    def __init__(self, username, email):
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
+        self.password = password
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
 
     def __repr__(self):
         return '<User %r>' % self.username
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 from flask_script import Manager,Shell
 from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
-from app.models import User
+from app.models import User,Role
 
 
 app = create_app('default')
@@ -12,7 +12,7 @@ manager = Manager(app)
 migrate = Migrate(app,db)
 
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
 
 manager.add_command("shell",Shell(make_context=make_shell_context))
 manager.add_command("db",MigrateCommand)
